5f6176b061babb028167fcf64987f342959ff8c2
HATAKEkakashi <hemant.kumar40fea@gmail.com>
2024-03-29 15:18:57 +0530

    variable

---
+++ basic/variable.js
@@ -0,0 +1,8 @@
+const account_id =12356
+let account_email="hemant.kumar@hotmail.com "
+var account_password="123456"
+account_city="Delhi"
+
+console.log(account_id);
+console.table([account_email,account_id,account_password,account_city]);
+/*don't use var in javascript bcz of the issue in block issue and functional isse*/ 
